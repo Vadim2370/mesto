70b54572776e28861ba006cadae4ff279c0a9be5
Vadim2370 <stdem11@yandex.ru>
2022-03-18 22:40:54 +1000

    Проектная работа 4 ревью 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,29 @@
-let elements = document.querySelectorAll('.element__like');
-
-for (let i = 0; i < elements.length; i = i + 1) {
-    elements[i].addEventListener('click', function() {
-        elements[i].classList.toggle('element__liked');
-    });
-};
-
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit');
 let formElement = document.querySelector('.popup__form');
 let closeButton = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileActivity = document.querySelector('.profile__activity');
-let nameInput = formElement.querySelector('#popup__input_type_name');
-let activiteInput = formElement.querySelector('#popup__input_type_activity');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let activiteInput = formElement.querySelector('.popup__input_type_activity');
 
-function popupOpen() {
-    popup.classList.toggle('popup__opened');
-    nameInput.value = profileName.textContent;
+function openPopup() {   //открываем форму кнопкой Edit
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;    //заполняем поля данными со страницы
     activiteInput.value = profileActivity.textContent;
 };
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupOpen);
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileActivity.textContent = activiteInput.value;
-    popupOpen();
+function closePopup() {    //закрываем форму кнопкой Close без сохранения
+    popup.classList.remove('popup_opened');
 };
 
-formElement.addEventListener('submit', formSubmitHandler);
+function saveFormPopup(evt) {    //сохраняем данные из полей формы на страницу и закрываем форму кнопкой Сохранить
+    evt.preventDefault();        //отменяем отправку формы и перезагрузку страницы
+    profileName.textContent = nameInput.value;
+    profileActivity.textContent = activiteInput.value;
+    closePopup();
+};
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', saveFormPopup);
